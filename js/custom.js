b0cc8427e3cd555058013791644277f8ebe88f83
hkkhchan <hkkhchan@gmail.com>
2016-09-16 00:13:00 +0800

    fix clip-path for fixfox

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,20 @@
 jQuery(document).ready(function(){
+		const $=jQuery;
 		var offset = 220;
 		var duration = 500;
-		jQuery(window).scroll(function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.cd-top').fadeIn(duration);
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > offset) {
+				$('.cd-top').fadeIn(duration);
 			} else {
-				jQuery('.cd-top').fadeOut(duration);
+				$('.cd-top').fadeOut(duration);
 			}
 		});
  
-		jQuery('.cd-top').click(function(event) {
+		$('.cd-top').click(function(event) {
 			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
+			$('html, body').animate({scrollTop: 0}, duration);
 			return false;
 		})
+		
+		
 });
