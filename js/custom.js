c52575b0da35dc05a8c39c458ae9e7e1abee507e
hkkhchan <hkkhchan@gmail.com>
2016-09-17 09:26:28 +0800

    js/css min

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,18 @@
 setTimeout(function(){
-		const $=jQuery;
-		var offset = 220;
-		var duration = 500;
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > offset) {
-				$('.cd-top').fadeIn(duration);
-			} else {
-				$('.cd-top').fadeOut(duration);
-			}
-		});
- 
-		$('.cd-top').click(function(event) {
-			event.preventDefault();
-			$('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		})
-		
-		
-},1000);
+        const $=jQuery;
+        var offset = 220;
+        var duration = 500;
+        //$(document).foundation();
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.cd-top').fadeIn(duration);
+            } else {
+                $('.cd-top').fadeOut(duration);
+            }
+        });
+        $('.cd-top').click(function(event) {
+            event.preventDefault();
+            $('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+},3000);
